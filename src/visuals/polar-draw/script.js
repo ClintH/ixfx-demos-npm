42a5820ca55ba2edb0d57efc318a283d0e984afe
CH <clint@thestaticvoid.net>
2024-03-24 16:35:34 +0100

    Updates to use CanvasHelper

--- src/visuals/polar-draw/script.js
+++ src/visuals/polar-draw/script.js
@@ -1,20 +1,25 @@
 import * as Dom from '../../ixfx/dom.js';
-import { scaler } from '../../ixfx/data.js';
-import { Oscillators } from '../../ixfx/modulation.js';
+import { scale, scaler } from '../../ixfx/data.js';
+import { Oscillators, perSecond } from '../../ixfx/modulation.js';
 import { continuously } from '../../ixfx/flow.js';
 import { Points, Polar, Scaler, degreeToRadian } from '../../ixfx/geometry.js';
 
 const settings = Object.freeze({
   /**
-   * Creates a scaler function for arm length.
-   * It will scale 0 ... 1 to a 0.1 ... 0.8 scale
-   * This means arm length minimum will be 10% and max 80%
+   * Scales 0..1 to 0.1..0.5
+   * This limits range of arm movement: 10% to 50%
    */
-  armLengthScale: scaler(0, 1, 0.1, 0.8),
+  armLengthScale: scaler(0, 1, 0.5, 0.5),
   /**
-   * Rotation per-frame, in radians
+   * Scales 0..1 to -150...50
+   * This limits range of arm angle movement (degrees)
    */
-  rotationPerFrame: degreeToRadian(0.1),
+  armAngleScale: scaler(0, 1, -170, -90),
+  /**
+   * Rotate once per second
+   */
+  rotationByTime: perSecond(degreeToRadian(0.1)),
+  drawRateMs: 50,
   /**
    * If true, the drawing arm is drawn
    */
@@ -24,177 +29,164 @@ const settings = Object.freeze({
  */
   armOrigin: { x: 0.5, y: 1 },
   // Sine wave oscillator that modulates length of drawing arm
-  osc: Oscillators.sine(100)
+  osc: Oscillators.sine(0.1),
+  osc2: Oscillators.sine(0.15),
+  overlayCanvas: new Dom.CanvasHelper(`#overlay`, {
+    fill: `viewport`, onResize: (size) => {
+      saveState({
+        scaleBy: Math.min(size.height, size.width)
+      });
+    }
+  }),
+  composedCanvas: new Dom.CanvasHelper(`#canvas`, { fill: `viewport` })
 });
 
 let state = Object.freeze({
-  /** @type number */
+  /** 
+   * Current canvas angle of rotation,
+   * in radians
+   * @type number */
   rotation: 0,
-  /**
-   * Current absolute position of arm.
-   * A line made up of two points.
-   */
-  armAbsolute: {
-    a: { x: 0, y: 0 },
-    b: { x: 0, y: 0 }
-  },
   /** 
    * Angle of arm in radians
    */
   armAngle: degreeToRadian(-90),
-  bounds: { width: 0, height: 0 },
+  /** 
+   * Computed end point of arm
+   * Relative coordinates
+   */
+  armEndAbsolute: { x: 0, y: 0 },
   /** @type number */
   scaleBy: 1,
-  /** @type {CanvasRenderingContext2D|undefined} */
-  drawingCtx: undefined,
-  middleAbs: { x: 0, y: 0 },
-  /** @type {OffscreenCanvas|undefined} */
-  bufferCanvas: undefined,
-  scale: Scaler.
+  armThickness: 20,
+  /** @type number */
+  lastDraw: 0
 });
 
 /**
  * Update state
- * This calculates everything necessary for drawing (in the 'use' function)
+ * This calculates everything necessary for drawing 
+ * (which happens in the 'use' function)
  */
 const update = () => {
-  const { rotationPerFrame, armOrigin, osc, armLengthScale } = settings;
-  const { rotation, scaleBy, armAngle } = state;
+  const { rotationByTime, armOrigin, osc, osc2, armLengthScale, armAngleScale } = settings;
+  const { rotation, scaleBy } = state;
 
-
-  // Default arm length - will be overriden by oscillator value
+  // Initial arm length - will be overriden by oscillator value
   let armLength = 0.5;
+  let armAngle = -90;
 
   // Modulate length of arm
   // 1. Get current value from the oscillator
-  const oscillatorValue = osc.next().value;
+  let oscillatorValue = osc.next().value;
   // 2. Scale value (if valid) according to the allowed range of the arm length
   if (oscillatorValue) {
-    armLength = armLengthScale(oscillatorValue);
+    armLength = armLengthScale(oscillatorValue) * scaleBy;
   }
 
-  // Relative coordinate of arm end
-  const armEnd = Polar.toCartesian(armLength, armAngle, armOrigin);
+  oscillatorValue = osc2.next().value;
+  if (oscillatorValue) {
+    armAngle = degreeToRadian(armAngleScale(oscillatorValue));
+  }
 
-  // Calculate absolute position of arm start & end
-  const armAbsolute = {
-    // Start
-    a: Points.multiplyScalar(armOrigin, scaleBy),
-    // End
-    b: Points.multiplyScalar(armEnd, scaleBy)
-  };
-
+  const armEndAbsolute = Polar.toCartesian(armLength, armAngle, Points.multiply(armOrigin, window.innerWidth, window.innerHeight));
 
   saveState({
-    // Save current arm position
-    armAbsolute,
-    // Increase rotation
-    rotation: rotation + rotationPerFrame
+    armEndAbsolute,
+    rotation: rotation + rotationByTime()
   });
 };
 
 const use = async () => {
-  const { drawingCtx, bounds, rotation, armAbsolute, middleAbs, bufferCanvas } = state;
+  const { armOrigin, composedCanvas, drawRateMs } = settings;
+  const { armEndAbsolute, rotation, lastDraw } = state;
 
-  if (!bufferCanvas) return; // No image buffer for some reason
-  const bufferCtx = bufferCanvas.getContext(`2d`);
-  if (!drawingCtx || !bufferCtx) return; // No drawing contexts
+  const ratio = composedCanvas.ratio;
 
-  // Draw on buffer
-  bufferCtx.clearRect(0, 0, bufferCanvas.width, bufferCanvas.height);
-  bufferCtx.fillStyle = `white`;
-  bufferCtx.font = `bold 48px serif`;
-  bufferCtx.fillText(`!Hello there`, 100, 0);
+  // Absolute position of arm start & end
+  const armAbsolute = {
+    // Start
+    a: Points.multiply(armOrigin, window.innerWidth, window.innerHeight),
+    // End
+    b: armEndAbsolute
+  };
 
-  // Draw buffer on canvas
-  drawingCtx.drawImage(bufferCanvas, 0, 200);
-  // Make a copy of the buffer
-  //  const bufferContents = await createImageBitmap(bufferCtx.createImageData(bounds.width, bounds.height));
+  const composedCtx = composedCanvas.ctx;
+  const off = new OffscreenCanvas(composedCanvas.width * ratio, composedCanvas.height * ratio);
+  const offCtx = off.getContext(`2d`);
+  if (offCtx) {
+    offCtx.scale(1 / ratio, 1 / ratio);
+    //offCtx.imageSmoothingQuality = `high`;
+    //offCtx.imageSmoothingEnabled = false;
+    offCtx.drawImage(composedCanvas.el, 0, 0);
+  }
+  composedCtx.clearRect(0, 0, composedCanvas.width, composedCanvas.height);
+  composedCtx.save();
+  composedCtx.imageSmoothingQuality = `high`;
+  composedCtx.imageSmoothingEnabled = false;
+  composedCtx.translate(composedCanvas.width / 2, composedCanvas.height / 2);
+  composedCtx.rotate(rotation);
+  composedCtx.drawImage(off, -composedCanvas.width / 2, -composedCanvas.height / 2);
+  composedCtx.restore();
 
-  //bufferCtx.drawImage(bufferContents, 0, 0);
-  //bufferCtx.reset();
-
-
-  //bufferCtx.resetTransform();
-  // Translate to arm end
-  // bufferCtx.save();
-  // bufferCtx.translate(armAbsolute.b.x, armAbsolute.b.y);
-  // drawAtArmEnd(bufferCtx);
-  // bufferCtx.resetTransform();
-
-  // // Translate & rotate canvas
-  // drawingCtx.save();
-  // drawingCtx.translate(middleAbs.x, middleAbs.y);
-  // drawingCtx.rotate(rotation);
-
-  // // Draw the buffer
-  // drawingCtx.drawImage(drawBuffer, 0, 0);
-
-  // // Undo transforms
-  // drawingCtx.resetTransform();
-
-  // if (settings.drawArm) drawArm(drawingCtx);
-
+  const elapsed = performance.now() - lastDraw;
+  if (elapsed >= drawRateMs) {
+    composedCtx.save();
+    composedCtx.translate(armAbsolute.b.x, armAbsolute.b.y);
+    drawAtArmEnd(composedCtx);
+    composedCtx.restore();
+    saveState({ lastDraw: performance.now() });
+  }
+  if (settings.drawArm) drawArm(armAbsolute);
 };
 
 /**
- * Draw whatever is meant to be at the tip of the drawing arm
- * @param {OffscreenCanvasRenderingContext2D} ctx
+ * Draw whatever is meant to be at the tip of the drawing arm.
+ * Drawing context is pre-transformed so that 0,0 is the
+ * end of the drawing arm.
+ * @param {CanvasRenderingContext2D} ctx
  */
 const drawAtArmEnd = (ctx) => {
+  const radius = 10;
   ctx.beginPath();
-  ctx.arc(0, 0, 10, 0, Math.PI * 2);
-  ctx.fillStyle = `yellow`;
-  ctx.fill();
+
+  // Draw a circle
+  //ctx.arc(-radius / 2, 0, radius, 0, Math.PI * 2);
+  //ctx.fillStyle = `hsla(${hue}, 100%, 50%, 0.1)`;
+  //ctx.fill();
+  ctx.strokeStyle = `hsla(${hue}, 100%, 50%, 0.1)`;
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, 50);
+  ctx.stroke();
   ctx.closePath();
+  hue += 0.1;
+  if (hue === 360) hue = 0;
+
 };
 
-/**
- * Draws the drawing arm
- * @param {CanvasRenderingContext2D} ctx
- * @returns
- */
-const drawArm = (ctx) => {
-  const { armAbsolute } = state;
+let hue = 0;
 
-  ctx.strokeStyle = `hsla(0,0,100%,0.1)`;
-  ctx.lineWidth = 10;
+/**
+ * Draws the arm of the drawing machine
+ * @param {import('../../ixfx/geometry.js').Line} armAbsolute
+ */
+const drawArm = (armAbsolute) => {
+  const { overlayCanvas } = settings;
+  const { armThickness } = state;
+  overlayCanvas.clear();
+  const { ctx } = overlayCanvas;
+  ctx.strokeStyle = `hsl(0,0%,30%)`;
+  ctx.lineWidth = armThickness;
   ctx.beginPath();
   ctx.lineCap = `round`;
   ctx.moveTo(armAbsolute.a.x, armAbsolute.a.y);
   ctx.lineTo(armAbsolute.b.x, armAbsolute.b.y);
-
   ctx.stroke();
   ctx.closePath();
 };
-
 function setup() {
-  const initBuffer = () => {
-    // Create a new offscreen canvas, making it larger than viewport
-    const c = new OffscreenCanvas(state.bounds.width, state.bounds.height);
-    saveState({
-      bufferCanvas: c
-    });
-  };
-
-  // Dom.fullSizeCanvas(`#canvas`, args => {
-  //   const scaleBy = Math.min(args.bounds.width, args.bounds.height);
-  //   saveState({
-  //     bounds: args.bounds,
-  //     drawingCtx: args.ctx,
-  //     scaleBy,
-  //     middleAbs: Points.multiplyScalar({ x: 0.5, y: 0.5 }, scaleBy)
-  //   });
-  //   initBuffer();
-  // });
-  resizeCanvas(`#canvas`, opts => {
-    saveState({
-      bounds: opts.size,
-      drawingCtx: opts.ctx,
-      scale: opts.scale
-    });
-  });
-
   continuously(async () => {
     update();
     await use();
@@ -202,31 +194,6 @@ function setup() {
 };
 setup();
 
-
-/**
- * 
- * @param {HTMLElement|string} domQueryOrEl 
- * @param {(opts:{size:{width:number,height:number}, ctx:CanvasRenderingContext2D, scale:Scaler.Scaler})=>void} callback 
- */
-function resizeCanvas(domQueryOrEl, callback) {
-  /** @type {HTMLCanvasElement} */
-  const el = Dom.resolveEl(domQueryOrEl);
-  const go = () => {
-    const size = el.getBoundingClientRect();
-    const ctx = el.getContext(`2d`);
-    el.width = size.width * devicePixelRatio;
-    el.height = size.height * devicePixelRatio;
-    ctx?.scale(devicePixelRatio, devicePixelRatio);
-    el.style.width = size.width + `px`;
-    el.style.height = size.height + `px`;
-
-    callback({
-      ...size, ctx, scale: Scaler.scaler(`min`, size)
-    });
-  };
-  window.addEventListener(`resize`, () => go());
-  go();
-}
 
 /**
  * Save state
